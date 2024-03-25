ce0fd99fe911fe046c83e539c032d9df8e1295ec
philipaantony <philipantony4u@gmail.com>
2024-03-25 21:05:23 +0530

    UI fixed

--- ShopKart/src/app/user/user-home/user-home.component.spec.ts
+++ ShopKart/src/app/user/user-home/user-home.component.spec.ts
@@ -3,6 +3,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserHomeComponent } from './user-home.component';
 
 describe('UserHomeComponent', () => {
+
+  
   let component: UserHomeComponent;
   let fixture: ComponentFixture<UserHomeComponent>;
 
